5e7a175df3be36e6b0d9263c0086790dcac997f0
Nicholas Yousefi <nsyousef@ucsd.edu>
2023-06-03 02:26:43 -0700

    Create custom element for about cards

---
+++ aboutcard.js
@@ -0,0 +1,117 @@
+const cardStyle = `
+article.about-card-left {
+  width: 100%;
+  display: grid;
+  grid-template-columns: 25% 75%;
+  grid-template-rows: 100%;
+}
+
+article.about-card-right {
+  width: 100%;
+  display: grid;
+  grid-template-columns: 75% 25%;
+  grid-template-rows: 100%;
+}
+
+img.profile-left {
+  width: 150px;
+  height: 150px;
+  border-radius: 100%;
+  grid-column-start: 1;
+  grid-column-end: 1;
+  align-self: center;
+  justify-self: center;
+}
+
+img.profile-right {
+  width: 150px;
+  height: 150px;
+  border-radius: 100%;
+  grid-column-start: 2;
+  grid-column-end: 2;
+  align-self: center;
+  justify-self: center;
+}
+
+h1 {
+  justify-self: center;
+}
+
+div.about-card-inner-holder-right {
+  grid-column-start: 2;
+  grid-column-end: 2;
+  grid-row-start: 1;
+  grid-row-end: 1;
+}
+
+div.about-card-inner-holder-left {
+  grid-column-start: 1;
+  grid-column-end: 1;
+  grid-row-start: 1;
+  grid-row-end: 1;
+}
+
+h2 {
+  font-size: 16pt;
+  font-weight: bolder;
+}
+`
+
+const cardProfileLeftHTML = `
+<img class="profile-left" src="profile.webp">
+<div class="about-card-inner-holder-right">
+  <h2 class="name">Name | Role</h2>
+  <p class="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti ab, debitis quos nam, ut impedit quae illo, veritatis unde dolorum enim ipsum. Cupiditate non voluptas quisquam maiores odit obcaecati omnis.</p>
+</div>
+`
+
+const cardProfileRightHTML = `
+<img class="profile-right" src="profile.webp">
+<div class="about-card-inner-holder-left">
+  <h2 class="name">Name | Role</h2>
+  <p class="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti ab, debitis quos nam, ut impedit quae illo, veritatis unde dolorum enim ipsum. Cupiditate non voluptas quisquam maiores odit obcaecati omnis.</p>
+</div>
+`
+
+class AboutCard extends HTMLElement {
+  constructor(profilePos) {
+    super();
+    let shadowDOM = this.attachShadow({mode: 'open'});
+    let article = document.createElement("article");
+    if (profilePos === "left") {
+      article.classList.add("about-card-left");
+    } else if (profilePos === "right") {
+      article.classList.add("about-card-right");
+    }
+    let style = document.createElement("style");
+    style.textContent = cardStyle;
+    shadowDOM.append(style, article);
+  }
+
+  /**
+   * @description This function sets the data in the card (profile pic, name, etc.)
+   * @param {Object} data - An object containing the data to put into the card in the following format:
+   *                        {
+   *                          "profileSrc": "string",
+   *                          "name": "string",
+   *                          "role": "string",
+   *                          "description": "string"
+   *                          "profilePos": "string"
+   *                        }
+   */
+  set data(data) {
+    if (!data) {
+      return;
+    }
+
+    let shadowDOM = this.shadowRoot;
+    let article = shadowDOM.lastChild;
+    if (article.classList[0] === "about-card-left") {
+      article.innerHTML = cardProfileLeftHTML;
+    } else if (article.classList[0] === "about-card-right") {
+      article.innerHTML = cardProfileRightHTML;
+    }
+  }
+}
+
+customElements.define('about-card', AboutCard);
---
+++ aboutus.js
@@ -0,0 +1,8 @@
+window.addEventListener('DOMContentLoaded', init);
+
+/**
+ * The main function that runs on this page
+ */
+function init() {
+  // TODO: create format for JSON file to hold people's info; parse that file and populate people's info into cards
+}
